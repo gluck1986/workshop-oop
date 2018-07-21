a9613fb1fd51c935097981c730a8e9027cbb6682
Kirill <gluck1986@gmail.com>
2018-07-21 20:06:21 +0400

    items

--- src/Services/Response/RssMaker.php
+++ src/Services/Response/RssMaker.php
@@ -10,14 +10,25 @@ namespace ConvertFeed\Services\Response;
 
 
 use ConvertFeed\Entity\Feed;
+use ConvertFeed\Entity\Item;
 
 class RssMaker
 {
     public function make(Feed $feed): string
     {
-        $simpleXml = new \SimpleXMLElement('<rss version="2.0"></rss>');
+        $simpleXml = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"></rss>');
+
         $chanel = $simpleXml->addChild('chanel');
         $chanel->addChild('title', $feed->title);
+        foreach ($feed->items as $item) {
+            /** @var Item $item */
+            $xmlItem = $chanel->addChild('item');
+            $xmlItem->addChild('title', $item->title);
+            $xmlItem->addChild('description', $item->description);
+            $xmlItem->addChild('link', $item->link);
+            $xmlItem->addChild('pubDate', $item->updated);
+            $xmlItem->addChild('guid', $item->id);
+        }
 
         return $simpleXml->asXML();
     }
